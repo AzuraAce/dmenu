1b3b19d77ce443900165fc359757424ccd9bd677
AzuraAce <oliverjbmub@gmail.com>
2025-09-23 12:31:18 +0200

    applied center

--- config.def.h
+++ config.def.h
@@ -2,12 +2,14 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static char font[] = "monospace:size=10";
 static const char *fonts[] = {
-	font,
-	"monospace:size=10",
+	font
 };
 
 static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
@@ -36,6 +38,7 @@ static const char worddelimiters[] = " ";
  */
 ResourcePref resources[] = {
 	{ "font",        STRING, &font },
+    { "fontalt0",    STRING, &fonts[1] },
 	{ "normfgcolor", STRING, &normfgcolor },
 	{ "normbgcolor", STRING, &normbgcolor },
 	{ "selfgcolor",  STRING, &selfgcolor },
